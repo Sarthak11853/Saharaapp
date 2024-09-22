91268ee452c9962f23d42f1b9d18dfc8e969de99
roshankrr <roshankrr16@gmail.com>
2024-09-22 09:41:07 +0530

    changes

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -59,7 +59,7 @@ useEffect(() => {
 
   const handleOTP=async()=>{
     try {
-        const res = await fetch("https://sahaara-q9i1l3rea-roshan-kumars-projects-6bfd45a7.vercel.app/api/otp", {
+        const res = await fetch("https://sahaara-app.vercel.app/api/otp", {
             method: "POST",
             body: JSON.stringify({location:location})
         })
